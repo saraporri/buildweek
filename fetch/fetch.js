cbf545cffa0344ed8ea6674768f5776025c5f900
Simone Giarnera <simone.giarnera@gmail.com>
2024-02-13 16:58:56 +0100

    creazione codice benchmark con fetch non funzionante

--- fetch/fetch.js
+++ fetch/fetch.js
@@ -1,13 +1,41 @@
-fetch("https://opentdb.com/api.php?amount=10&category=18&difficulty=easy")
-  .then((response) => response.json())
-  .then((data) => {
-    // Manipolazione del DOM basata sui dati ottenuti
-    data.results.forEach((item) => {
-      const element = document.createElement("h3");
-      element.innerText = item.question;
-      document.querySelector(".domanda").appendChild(element);
+let currentQuestionIndex = 0;
+const apiUrl =
+  "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy";
+
+function fetchQuestion() {
+  fetch(apiUrl)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Errore nella richiesta!");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      if (currentQuestionIndex < data.results.length) {
+        const question = data.results[currentQuestionIndex];
+        displayQuestion(question);
+        currentQuestionIndex++;
+      }
+    })
+    .catch((error) => {
+      console.error("Si è verificato un errore:", error);
     });
-  })
-  .catch((error) => {
-    console.error("Si è verificato un errore:", error);
+}
+
+const questionElement = document.createElement("h3");
+
+function displayQuestion(question) {
+  questionElement.textContent = question.question; // Correzione qui
+  document.querySelector(".domanda").appendChild(questionElement); // Assicurati che l'elemento esista
+}
+
+// Assumi che questa sia l'unica volta che vuoi avviare fetchQuestion all'inizio.
+fetchQuestion();
+
+// Aggiungi un listener solo se è necessario caricare nuove domande al click
+document.querySelectorAll(".box").forEach((box) => {
+  box.addEventListener("click", function () {
+    questionElement.remove();
+    fetchQuestion();
   });
+});
