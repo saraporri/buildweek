1df5667a0fec2b380018abc257097306d9887e10
OttPFC <ale.maz097@gmail.com>
2024-02-13 16:08:34 +0100

    countdown e cambio template

--- js/checkboxbutton.js
+++ js/checkboxbutton.js
@@ -1,11 +1,21 @@
 const bottone = document.querySelector(".bottone");
 const checkbox = document.querySelector("#checkbox");
 const error = document.querySelector(".error");
+let welcome = document.querySelector('.container')
 
 bottone.addEventListener("click", function () {
   if (checkbox.checked) {
-    alert("bravo");
+    let temp = document.getElementsByTagName("template")[0];
+    let clon = temp.content.cloneNode(true);
+    document.body.appendChild(clon);
+    document.body.removeChild(welcome);
   } else {
     error.classList.replace("error", "error-red");
   }
 });
+
+
+
+
+
+
