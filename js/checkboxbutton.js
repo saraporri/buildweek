2380318dde04e4af226dde7fac56b5eb28cb51cc
saraporri <saraporricino@yahoo.com>
2024-02-13 12:44:48 +0100

    changed style

--- js/checkboxbutton.js
+++ js/checkboxbutton.js
@@ -1,12 +1,11 @@
 const bottone = document.querySelector(".bottone");
 const checkbox = document.querySelector("#checkbox");
-const error = document.querySelector("#error");
-error.style.display = "none";
+const error = document.querySelector(".error");
 
 bottone.addEventListener("click", function () {
   if (checkbox.checked) {
     alert("bravo");
   } else {
-    error.style.display = "block";
+    error.classList.replace("error", "error-red");
   }
 });
